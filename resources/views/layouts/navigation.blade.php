be3fdda10e86168f4041963bf9ef157c017d3c0c
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2025-10-08 20:36:15 +0200

    Added Casher logo and analysis tab route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('analysis')" :active="request()->routeIs('analysis')">
+                        {{ __('Analysis') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/analysis', function () {
+    return view('analysis');
+})->middleware(['auth', 'verified'])->name('analysis');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
