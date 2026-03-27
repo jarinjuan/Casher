32d86ca99a9331eb81abca3723b0fd23d63c74b9
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2026-03-28 00:01:33 +0100

    Implemented e-mail validation

---
+++ lang/cs/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'email' => 'Pole :attribute musí být platná e-mailová adresa.',
+    'required' => 'Pole :attribute je povinné.',
+    'string' => 'Pole :attribute musí být řetězec.',
+    'max' => [
+        'string' => 'Pole :attribute nesmí být delší než :max znaků.',
+    ],
+    'unique' => 'Tento :attribute již existuje.',
+    'confirmed' => 'Potvrzení pole :attribute se neshoduje.',
+    'attributes' => [
+        'email' => 'e-mail',
+        'password' => 'heslo',
+        'name' => 'jméno',
+    ],
+];
---
+++ lang/en/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'email' => 'The :attribute field must be a valid email address.',
+    'required' => 'The :attribute field is required.',
+    'string' => 'The :attribute field must be a string.',
+    'max' => [
+        'string' => 'The :attribute field must not be greater than :max characters.',
+    ],
+    'unique' => 'The :attribute has already been taken.',
+    'confirmed' => 'The :attribute field confirmation does not match.',
+    'attributes' => [
+        'email' => 'email',
+        'password' => 'password',
+        'name' => 'name',
+    ],
+];
